274f7758423e6b353aaf1a7952349c0f9b9458bc
Hua <a47894785@gmail.com>
2022-12-07 22:38:47 +0800

    1207 init commit

--- FWC/ViewController.swift
+++ FWC/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connectBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func navegateToWebsite(_ sender: Any) {
+        if let url	= URL(string: "https://www.youtube.com/?hl=zh-tw") {
+            UIApplication.shared.open(url)
+        }
+        
+        
+        
+    }
 }
-
